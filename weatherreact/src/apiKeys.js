f8047828eb4874e23e3efee09756e3b4a693beb5
yuvraj625 <nictesla822@gmail.com>
2023-05-27 13:35:54 +0530

    Weather app using react

---
+++ weatherreact/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "{3079d48a7803b90c7b57f3b08396bf74}",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
